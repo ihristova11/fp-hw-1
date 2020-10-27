e17b737be9aeb4d8eb0212e042ba829c2447e8ad
Irina Hristova <hristova119@gmail.com>
2020-10-28 01:05:54 +0200

    Update 1.scm

--- 1.scm
+++ 1.scm
@@ -5,109 +5,38 @@
 (define h "\u2500")
 (define v "\u2502")
 
+(define (++ i) (+ 1 i))
+
 (define (accumulate op term init a next b)
   (define (loop i result)
     (if (< i b)
         (loop (next i) (op result (term i)))
         result))
-  (loop a init)
-)
+  (loop a init))
 
-(define (top i b)
-  (cond ((= i 0) dr)
-      ((= i (- b 1)) dl)
-      (else h)
-   )
-)
-
-(define (bottom i b)
-  (cond ((= i 0) ur)
-      ((= i (- b 1)) ul)
-      (else h)
-   )
-)
-
+(define (position i n ch1 ch2)
+  (cond ((= i 0) ch1)
+      ((= i (- n 1)) ch2)
+      (else h)))
 
 (define (append-symbols ch1 ch2 n)
-  (accumulate string-append (lambda (i)(string-append ch1 ch2)) "" 0 (lambda (x)(+ 1 x)) n)
-)
+  (accumulate string-append (lambda (i)(string-append ch1 ch2)) "" 0 ++ n))
 
-(define (draw-top-line n)
-  (accumulate string-append (lambda (x)(top x (+ 1 (* 2 n)))) "" 0 (lambda (x) (+ 1 x)) n)
-)
+(define (append-position-line n func)
+  (accumulate string-append func "" 0 ++ n))
 
-(define (draw-bottom-line n)
-  (accumulate string-append bottom "" 0 (lambda (x) (+ 1 x)) n)
-)
+(define (append-half position y z ch1 ch2)
+  (string-append (append-symbols v " " y) (append-position-line z (lambda (x)(position x z ch1 ch2))) (append-symbols " " v y) "\n"))
 
-(define (draw-square n)
- (display (draw-top-line n))
- (display (draw-bottom-line n))
-)
-
-(define (draw-top n)
-  (define (loop i)
+(define (draw-position-half n append-half position term1 term2 ch1 ch2)
+   (define (loop i)
     (if (< i n)
-       (begin (display (string-append (append-symbols v " " i)
-                                      (draw-top-line (+ (* 2 (- n i)) 1))
-                                      (append-symbols " " v i) "\n"))
-              (loop (+ 1 i))
-       )
-    )
-  )
-
-  (loop 0)
-)
-
-(define (draw-bottom n)
-  (define (loop i)
-    (if (< i n)
-       (begin (display (string-append (append-symbols v " " (- n i)) (draw-bottom-line (+ (* 4 i) 1)) (append-symbols " " v (- n i)) "\n"))
-              (loop (+ 1 i))
-       )
-    )
-  )
-
-  (loop 0)
-)
-
-
-
+       (begin (display (append-half position (term1 i) (term2 i) ch1 ch2)) 
+              (loop (++ i)))))
+  (loop 0))
 
 (define (squares n)
-  (draw-top n)
-  ;(draw-bottom n)
-)
+  (draw-position-half n append-half position (lambda (i) i) (lambda (i)(+ 3 (* 4 (- (- n i) 1)))) dr dl)
+  (draw-position-half n append-half position (lambda (i)(- (- n i) 1)) (lambda (i)(+ 3 (* 4 i))) ur ul))
 
 (squares 3)
-
-(display "*")
-
-;(draw-square 5)
-
-; todo: space to be a defined symbol
-;(draw-line "")
-; 2*n+1 // символи от ъгъл до ъгъл, n - номер на квадрата от вътре навън
-
-;┌─────────┐
-;│ ┌─────┐ │
-;│ │ ┌─┐ │ │
-;│ │ └─┘ │ │
-;│ └─────┘ │
-;└─────────┘
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
