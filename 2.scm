cbd23ad2149c65438374547bec09a6bf361f9540
Irina Hristova <hristova119@gmail.com>
2020-10-29 15:30:04 +0200

    Update 2.scm

--- 2.scm
+++ 2.scm
@@ -17,21 +17,19 @@
 (define (w i) (+ 1 i))
 (define (p i) (* 2 i))
 
-(define (++ i) (+ 1 i))
-
 (define (knapsack c n w p)
   (define (sum_elements set index result) ; refactor
-    (define (get_value index) (if (set-contains? set (expt 2 (++ index))) (p index) 0))
+    (define (get_value index) (if (set-contains? set (expt 2 (+ 1 index))) (p index) 0))
     (if (< index n)
-        (sum_elements set (++ index) (+ result  (get_value index)))
+        (sum_elements set (+ 1 index) (+ result  (get_value index)))
         result
     )
   )
 
   (define (sum_weight set index result) ; refactor
-    (define (get_value index) (if (set-contains? set (expt 2 (++ index))) (w index) 0))
+    (define (get_value index) (if (set-contains? set (expt 2 (+ 1 index))) (w index) 0))
     (if (< index n)
-        (sum_weight set (++ index) (+ result (get_value index)))
+        (sum_weight set (+ 1 index) (+ result (get_value index)))
         result
     )
   )
@@ -39,16 +37,14 @@
   (define (find_max_price max_price current_set)
      (if (and (<= (sum_weight current_set 0 0) c) (> (sum_elements current_set 0 0) max_price))
                      (sum_elements current_set 0 0)
-                     max_price)
-  )
+                     max_price))
 
   (define (find_max_price_set max_price current_set)
-    (if (and (<= (sum_weight current_set 0 0) c) (> (sum_elements current_set 0 0) max_price)) current_set)
-  )
+    (if (and (<= (sum_weight current_set 0 0) c) (> (sum_elements current_set 0 0) max_price)) current_set))
   
   (define (loop current_set max_price max_price_set)
-    (if (< current_set (+ 1 (expt 2 n))) ; i < 2^n + 1 всички възможности
-        (loop (++ current_set) (find_max_price max_price current_set) (find_max_price_set max_price current_set))
+    (if (< current_set (- 1 (expt 2 n))) ; i < 2^n + 1 всички възможности
+        (loop (+ 1 current_set) (find_max_price max_price current_set) (find_max_price_set max_price current_set))
         max_price_set
     )
   )
@@ -57,4 +53,4 @@
 )
 
 
-(knapsack 10 3 w p)
+(knapsack 24 5 w p)
