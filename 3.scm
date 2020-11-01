dd41b6732d7d2f3fcfc6bbb76ea79d6892d0f698
Irina Hristova <hristova119@gmail.com>
2020-11-01 10:18:46 +0200

    Update 3.scm

---
+++ 3.scm
@@ -0,0 +1,52 @@
+(define (positive-number? n) (and (integer? n) (< 0 n)))
+
+;(define (operator? c) (or (string=? c '+') (string=? c '-') (string=? c '*') (string=? c '/') (string=? c '^')))
+
+(define (expr-valid? expr)
+  #t
+ ; (cond ((= "" expr) #t)
+ ;       ((positive-number? expr) #t)
+ ;       (else #t)) ; work
+        
+)
+
+(define (expr-rp expr)
+  (if (not (expr-valid? expr)) #f
+      #t ; work
+  )
+)
+
+(define (expr-eval expr)
+   (if (not (expr-valid? expr)) #f
+      #t ; work
+  )
+)
+
+(define (tail str) (substring str 1 (string-length str)))
+(define (head str) (substring str 0 1))
+
+(define (>> str)
+  (define (loop str result)
+    (if (string=? " " (head str))
+        result
+        (loop (tail str) (string-append result (head str)))
+    )
+  )
+  (if (not (expr-valid? str)) #f (loop str ""))
+)
+
+;(define (string-trim expr)
+  
+ ; (define (loop expr result lastnumber)
+  ;  (cond ((= expr "") result)
+   ;       ((and lastnumber (positive-number? (head expr))) (loop (tail expr) result #t))
+    ;      ((and lastspace (not (= " " (head expr)))) )
+     ;     (else (loop (tail expr) (string-append result (head expr)) #t))
+       
+ ; )
+;)
+
+(>> "   in")
+(>> "in the wood")
+
+;(string-trim "  in the end  ")(string-trim "  in the end  ")          =>  "in the end"
