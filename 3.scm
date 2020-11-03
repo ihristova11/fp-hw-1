8efa7e3a2df31ec6315f924ab9b6749dda8a6475
Irina Hristova <hristova119@gmail.com>
2020-11-04 00:01:03 +0200

    Update 3.scm

--- 3.scm
+++ 3.scm
@@ -46,9 +46,10 @@
 
 (define (expr-valid? expr)
  (define (loop str numcount opcount)
-   (cond ((string=? "" (>> str)) #t)
-         ((< numcount opcount) #f)
+   ;(display (string-append "str:" str "| >>str:" (>> str) "|\n"))
+   (cond ((< numcount opcount) #f)
          ((< (+ 1 opcount) numcount) #f)
+         ((string=? "" (>> str)) #t)
          ((operator? (>> str)) (loop (tail str (>>len str)) numcount (+ 1 opcount)))
          ((positive-number? (>> str)) (loop (tail str (>>len str)) (+ 1 numcount) opcount))
          (else #f)
@@ -59,28 +60,29 @@
 
 (define (pop str) (tail (reverse-string str) (>>len (reverse-string str))))
 (define (top str) (reverse-string (>> (reverse-string str))))
-(define (push str el) (string-append str " " el))
+(define (push str el delim) (string-append str delim el))
 
 (define (expr-rp expr)
   (define (form-expr out op)
-    (display (string-append "out:" out "| op:" op "| " ">>op:" (>> op) "|\n"))
+    ;(display (string-append "out:" out "| op:" op "| " ">>op:" (>> op) "|\n"))
     (if (string=? (>> op) "")
         (tail out 1) ; remove empty space at the beggining
-        (form-expr (push out (top op)) (pop op))
+        (form-expr (push out (top op) "") (pop op))
     )
   )
   
   (define (loop str out op)
     ;(display (string-append "str:" str "|out:" out "|op:" op "|\n" "|>>str:" (>> str) "|\n"))
-    (cond ((string=? "" str) (form-expr out op))
-          ((positive-number? (>> str)) (loop  (tail str (>>len str)) (push out (>> str)) op))
-          ((and (operator? (>> str)) (string=? "" op)) (loop (tail str (>>len str)) out (push op (>> str)))) ; empty op stack, just add operator
-          ((and (operator? (>> str)) (< (precedence (top op)) (precedence (>> str)))) (loop (tail str (>>len str)) out (push op (>> str)))) ; operator in stack has < priority than the currently read, push the new operator as well (TODO: CHECK EQUAL PRIORITY | LEFT)
-          ((and (operator? (>> str)) (>= (precedence (top op)) (precedence (>> str))))  ((lambda ()
-                                                                                               (if (>= (precedence (top (pop op))) (precedence (>> str)))
-                                                                                                   (loop (tail str (>>len str)) (push (push out (top op)) (top (pop op))) (push (pop (pop op)) (>> str)))
-                                                                                                   (loop (tail str (>>len str)) (push out (top op)) (push (pop op) (>> str)))
-                                                                                               )))) ; high priority-> pop and then push the new op
+    (cond ((string=? "" (>> str)) (form-expr out op))
+          ((positive-number? (>> str)) (loop  (tail str (>>len str)) (push out (>> str) ",") op))
+          ((and (operator? (>> str)) (string=? "" op)) (loop (tail str (>>len str)) out (push op (>> str) " "))) ; empty op stack, just add operator
+          ((and (operator? (>> str)) (< (precedence (top op)) (precedence (>> str)))) (loop (tail str (>>len str)) out (push op (>> str) " "))) ; operator in stack has < priority than the currently read, push the new operator as well (TODO: CHECK EQUAL PRIORITY | LEFT)
+          ((and (operator? (>> str)) (>= (precedence (top op)) (precedence (>> str))))
+           ((lambda ()
+              (if (>= (precedence (top (pop op))) (precedence (>> str)))
+                  (loop (tail str (>>len str)) (push (push out (top op) "") (top (pop op)) "") (push (pop (pop op)) (>> str) " "))
+                  (loop (tail str (>>len str)) (push out (top op)) (push (pop op) (>> str) " ") "")
+              )))) ; high priority-> pop and then push the new op
     )
   )
   (if (expr-valid? expr)
@@ -91,7 +93,11 @@
 
 
 
-(expr-rp "10+  20 * 30")
+;(expr-rp "")
+;(expr-rp "  5;  ")
+;(positive-number? (>>  "   10    "))
+
+;(expr-valid? "   10    ")
 
 ;(expr-valid? "10   * 20")
 ;(expr-valid? "10 20 + 5+")
