e7b84f5c8005f199e59ac24573be6ceffecc17b4
Irina Hristova <hristova119@gmail.com>
2020-11-04 10:17:14 +0200

    Update 3.scm

--- 3.scm
+++ 3.scm
@@ -62,26 +62,33 @@
 (define (top str) (reverse-string (>> (reverse-string str))))
 (define (push str el delim) (string-append str delim el))
 
+;bug to fix: 
+
 (define (loop str out op delim_out delim_op delim_form)
     
   (define (form-expr out op)
-    ;(display (string-append "out:" out "| op:" op "| " ">>op:" (>> op) "|\n"))
+    (display (string-append "form-expr||out:" out "| op:" op "| " ">>op:" (>> op) "|\n"))
     (if (string=? (>> op) "")
         (tail out 1) ; remove empty space at the beggining
         (form-expr (push out (top op) delim_form) (pop op))
     )
   )
-    ;(display (string-append "str:" str "|out:" out "|op:" op "|\n" "|>>str:" (>> str) "|\n"))
+
+  (define (loop1 str out op delim_out delim_op delim_form)
+    (display (string-append "loop1||| str:" str "|out:" out "|op:" op "|\n"))
+    (if (or (< (precedence (top op)) (precedence (>> str))) (string=? "" (top op)))
+    (loop (tail str (>>len str)) out (push op (>> str) " ") delim_out delim_op delim_form)
+    (loop1 str (push out (top op) "") (pop op) delim_out delim_op delim_form))
+  )
+
+  
+    (display (string-append "str:" str "|out:" out "|op:" op "|\n" "|>>str:" (>> str) "|\n"))
     (cond ((string=? "" (>> str)) (form-expr out op))
           ((positive-number? (>> str)) (loop  (tail str (>>len str)) (push out (>> str) delim_out) op delim_out delim_op delim_form))
           ((and (operator? (>> str)) (string=? "" op)) (loop (tail str (>>len str)) out (push op (>> str) delim_op) delim_out delim_op delim_form)) ; empty op stack, just add operator
           ((and (operator? (>> str)) (< (precedence (top op)) (precedence (>> str)))) (loop (tail str (>>len str)) out (push op (>> str) delim_op) delim_out delim_op delim_form)) ; operator in stack has < priority than the currently read, push the new operator as well (TODO: CHECK EQUAL PRIORITY | LEFT)
           ((and (operator? (>> str)) (>= (precedence (top op)) (precedence (>> str))))
-           ((lambda ()
-              (if (>= (precedence (top (pop op))) (precedence (>> str)))
-                  (loop (tail str (>>len str)) (push (push out (top op) "") (top (pop op)) "") (push (pop (pop op)) (>> str) delim_op) delim_out delim_op delim_form)
-                  (loop (tail str (>>len str)) (push out (top op)) (push (pop op) (>> str) delim_op) "" delim_out delim_op delim_form)
-              )))) ; high priority-> pop and then push the new op
+           (loop1 str out op delim_out delim_op delim_form)) ; high priority-> pop and then push the new op
     )
   )
 
@@ -100,7 +107,7 @@
   (cond ((string=? "*" operation) (* operand1 operand2))
         ((string=? "+" operation) (+ operand1 operand2))
         ((string=? "-" operation) (- operand1 operand2))
-        ((string=? "/" operation) (/ operand1 operand2)) ; or quotation
+        ((string=? "/" operation) (/ operand1 operand2)) ; quotation
         ((string=? "^" operation) (expt operand1 operand2))
   )
 )
@@ -127,17 +134,19 @@
 
 ;(expr-valid? "   10    ")
 
-(expr-eval "10   * 20")
-(expr-eval "10* 20 + 5")
+;(expr-eval "10   * 20")
+;(expr-eval "10* 20 + 5")
 ;(expr-valid? "++++ 5")
 ;(expr-valid? "+++")
 ;(expr-valid? "")
 ;(expr-valid? "-5")
 ;(expr-valid? "-5 + 2 * 2 2")
-(expr-eval "3")
-(expr-eval "10 + 5* 2")
-(expr-eval " 10*10+   10 * 5^ 2")
-(expr-eval "10/5*2")
-(expr-eval "    10   ")
-(expr-eval "   ")
+;(expr-eval "3")
+;(expr-eval "10 + 5* 2")
+;(expr-rp " 10*10+   10 * 5^ 2")
+;(expr-eval "10/5*2")
+;(expr-eval "    10   ")
+;(expr-eval "   ")
 
+(expr-eval "10+5*8/5^2+4/5-8")
+;(expr-rp "10+20*30/2^2+2^10")
