8fb72e9549443759a5c118e205b1111b69b867f0
Irina Hristova <hristova119@gmail.com>
2020-11-04 12:22:43 +0200

    Update 3.scm

--- 3.scm
+++ 3.scm
@@ -49,14 +49,12 @@
 
 (define (expr-valid? expr)
  (define (loop str numcount opcount)
-   ;(display (string-append "str:" str "| >>str:" (read-token str) "|\n"))
    (cond ((< numcount opcount) #f)
          ((< (+ 1 opcount) numcount) #f)
          ((string=? "" (read-token str)) #t)
          ((operator? (read-token str)) (loop (tail str (read-token-len str)) numcount (+ 1 opcount)))
          ((natural-number? (read-token str)) (loop (tail str (read-token-len str)) (+ 1 numcount) opcount))
-         (else #f)
-   )
+         (else #f))
  )
   (loop expr 0 0)
 )
@@ -65,53 +63,40 @@
 (define (top str) (reverse-string (read-token (reverse-string str))))
 (define (push str el delim) (string-append str delim el))
 
-;bug to fix: 
-
 (define (loop str out op delim_out delim_op delim_form)
-    
-  (define (form-expr out op)
-    ;(display (string-append "form-expr||out:" out "| op:" op "| " ">>op:" (read-token op) "|\n"))
-    (if (string=? (read-token op) "")
-        (tail out 1) ; remove empty space at the beggining
-        (form-expr (push out (top op) delim_form) (pop op))
-    )
-  )
+  (define (form-expr out op) (if (string=? (read-token op) "") (tail out 1) (form-expr (push out (top op) delim_form) (pop op))))
 
   (define (loop1 str out op delim_out delim_op delim_form)
-    ;(display (string-append "loop1||| str:" str "|out:" out "|op:" op "|\n"))
     (if (or (< (precedence (top op)) (precedence (read-token str))) (string=? "" (top op)))
     (loop (tail str (read-token-len str)) out (push op (read-token str) " ") delim_out delim_op delim_form)
     (loop1 str (push out (top op) " ") (pop op) delim_out delim_op delim_form))
   )
 
-  
-    ;(display (string-append "str:" str "|out:" out "|op:" op "|\n" "|>>str:" (read-token str) "|\n"))
-    (cond ((string=? "" (read-token str)) (form-expr out op))
-          ((natural-number? (read-token str))
-           (loop  (tail str (read-token-len str)) (push out (read-token str) delim_out) op delim_out delim_op delim_form))
-          ((and (operator? (read-token str)) (string=? "" op))
-           (loop (tail str (read-token-len str)) out (push op (read-token str) delim_op) delim_out delim_op delim_form)) ; empty op stack, just add operator
-          ((and (operator? (read-token str)) (< (precedence (top op)) (precedence (read-token str))))
-           (loop (tail str (read-token-len str)) out (push op (read-token str) delim_op) delim_out delim_op delim_form)) ; operator in stack has < priority than the currently read, push the new operator as well (TODO: CHECK EQUAL PRIORITY | LEFT)
-          ((and (operator? (read-token str)) (>= (precedence (top op)) (precedence (read-token str))))
-           (loop1 str out op delim_out delim_op delim_form)) ; high priority-> pop and then push the new op
-    )
-  )
+  (cond ((string=? "" (read-token str)) (form-expr out op))
+        ((natural-number? (read-token str))
+         (loop  (tail str (read-token-len str)) (push out (read-token str) delim_out) op delim_out delim_op delim_form))
+        ((and (operator? (read-token str)) (string=? "" op)) ; empty op stack
+         (loop (tail str (read-token-len str)) out (push op (read-token str) delim_op) delim_out delim_op delim_form)) 
+        ((and (operator? (read-token str)) (< (precedence (top op)) (precedence (read-token str)))) ; operator in stack has < priority 
+         (loop (tail str (read-token-len str)) out (push op (read-token str) delim_op) delim_out delim_op delim_form)) 
+        ((and (operator? (read-token str)) (>= (precedence (top op)) (precedence (read-token str)))) ; higher priority
+         (loop1 str out op delim_out delim_op delim_form)) 
+   )
+)
 
 
   
 (define (expr-rp expr) (if (expr-valid? expr) (loop expr "" "" "," " " " ") #f))
 
-(define (eval-op operation operand1 operand2)
+(define (expr-eval expr)
+  (define (eval-op operation operand1 operand2)
   (cond ((string=? "*" operation) (* operand1 operand2))
         ((string=? "+" operation) (+ operand1 operand2))
         ((string=? "-" operation) (- operand1 operand2))
         ((string=? "/" operation) (quotient operand1 operand2))
         ((string=? "^" operation) (expt operand1 operand2))))
-
-(define (expr-eval expr)
+  
   (define (func str out)
-    ;(display (string-append "str:" str "|out:" out "|sum:")) (display sum) (display "|\n")
     (cond ((and (string=? "" out) (string=? "" (read-token str))) 0)
           ((string=? "" (read-token str)) (string->number (read-token out)))
           ((natural-number? (read-token str)) (func (tail str (read-token-len str)) (push out (read-token str) " ")))
@@ -122,10 +107,7 @@
   (if (expr-valid? expr) (func (loop expr "" "" " " " " " ") "") #f)
 )
 
-(expr-eval "")
-(expr-rp "  5;  ")
-
-;(expr-valid? "   10    ")
+(expr-valid? "   10    ")
 
 (expr-eval "10   * 20")
 (expr-eval "10* 20 + 5")
@@ -143,5 +125,5 @@
 
 
 ;(quotient 11 0)
-;(expr-eval "0+4/5*5/0+2+4/5-8")
+(expr-eval "10+5*8/5^2+4/5-8")
 (expr-eval "10+20*30+5*12^4/2^2+2^10")
