8790e0ae2f4ff41480bd7322595cffee494aedb4
Irina Hristova <hristova119@gmail.com>
2020-11-06 19:36:30 +0200

    Create reverse-num.rkt

---
+++ test-1-prep/reverse-num.rkt
@@ -0,0 +1,49 @@
+;Да се напише функция (!! n), която по дадено естествено число n изчислява n!! - произведението на всички числа,
+;по-малки или равни на n, със същата четност:
+
+(define (id x) x)
+(define (1+ x) (+ 1 x))
+
+(define (accumulate from to op term next acc)
+  (if (> from to)
+      acc
+      (accumulate (next from) to op term next (op acc (term from)))
+  )
+)
+
+(define (accumulate-i op term init a next b)
+  (define (loop i result)
+    (if (< b i )
+        (loop (next i) (op result (term i)))
+        result))
+  (loop a init))
+
+(define (!! n)
+  (accumulate ((lambda () (if (odd? n) 1 2))) n (lambda (x y) (* x y)) id (lambda (x) (+ x 2)) 1)
+)
+
+(define (fact n)
+  (accumulate 1 n (lambda (x y) (* x y)) id 1+ 1)
+)
+
+(define (rev-num n)
+  (accumulate-i (lambda (x y) (+ (* x 10) y)) (lambda (x) (remainder x 10)) 0 n (lambda (x) (quotient x 10)) 0))
+
+(define (nchk n k)
+  (/ (fact n) (* (fact k) (fact (- n k))))
+)
+
+(define (nchk* n k)
+  (accumulate 0 (- k 1) * (lambda (i) (/ (- n i) (- k i))) 1+ 1)
+)
+
+(define (2^ n)
+  (accumulate 1 n * (lambda (i) 2) 1+ 1)
+)
+
+(fact 10)
+(!! 4)
+(nchk 3 2)
+(2^ 3)
+
+;(rev-num 12345)
