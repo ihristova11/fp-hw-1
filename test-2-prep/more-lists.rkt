d83c3314a9731a23dd00f9ca3e8c3ae8c0a73921
Irina Hristova <hristova119@gmail.com>
2020-11-28 19:28:24 +0200

    Update more-lists.rkt

--- test-2-prep/more-lists.rkt
+++ test-2-prep/more-lists.rkt
@@ -1,9 +1,21 @@
 #lang racket
+(require rackunit)
+(require rackunit/text-ui)
 
 (define (takeWhile p? xs)
-  (cond ((p? (car xs)) (cons (car xs) (takeWhile p? (cdr xs))))
-        ((or (null? xs) (not (p? (car xs)))) '())
+  (cond ((null? xs) '())
+        ((p? (car xs)) (cons (car xs) (takeWhile p? (cdr xs))))
+        (else '())
   )
 )
 
-(takeWhile even? '(2 4 6 8 9 10 11 12))
+
+(define tests
+  (test-suite "take while"
+    (check-equal? (takeWhile even? '(2 4 6 8 9 10 11 12)) '(2 4 6 8))
+    (check-equal? (takeWhile even? '(1 2 4 6 8 9 10 11 12)) '())
+    (check-equal? (takeWhile even? '()) '())
+  )
+)
+
+(run-tests tests 'verbose)
