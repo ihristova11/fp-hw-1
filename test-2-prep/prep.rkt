e3c8ae7ac1bc30cce96c023dbde73d1b8a190d71
Irina Hristova <hristova119@gmail.com>
2020-12-10 16:45:00 +0200

    Update prep.rkt

--- test-2-prep/prep.rkt
+++ test-2-prep/prep.rkt
@@ -1,11 +1,22 @@
 #lang racket/base
 
-(define (map op lst)
+(define (map-mine op lst)
   (if (null? lst)
       '()
-      (cons (op (car lst)) (map op (cdr lst)))
+      (cons (op (car lst)) (map-mine op (cdr lst)))
+  )
+)
+
+(define (filter-mine p? lst)
+  (cond ((null? lst) '())
+        ((p? (car lst)) (cons (car lst) (filter-mine p? (cdr lst))))
+        (else (filter-mine p? (cdr lst)))
   )
 )
 
 
-(map (lambda (x) (* x 2)) (list 1 2 3 4))
+(map-mine (lambda (x) (* x 2)) (list 1 2 3 4))
+
+(map + '(1 2 3) '(10 10 10))
+
+(filter-mine even? '(1 2 3 4))
