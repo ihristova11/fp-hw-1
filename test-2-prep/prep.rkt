e1bd9a88ae93a14fdba6d584ab8a934f3b5b1c4a
Irina Hristova <hristova119@gmail.com>
2020-12-11 13:37:45 +0200

    Update prep.rkt

--- test-2-prep/prep.rkt
+++ test-2-prep/prep.rkt
@@ -20,3 +20,61 @@
 (map + '(1 2 3) '(10 10 10))
 
 (filter-mine even? '(1 2 3 4))
+
+; define flatten
+
+(define empty-tree '())
+(define (make-tree root left right) (list root left right))
+(define (make-leaf root) (make-tree root empty-tree empty-tree)) ; (list root empty-tree empty-tree)
+
+(define root-tree car) ; check if car exists
+(define left-tree cadr)
+(define right-tree caddr) ; check?
+
+(define (tree? t)
+  (cond ((null? t) #t)
+        ((and
+          (list? t)
+          (= (length t) 3)
+          (tree? (right-tree t))
+          (tree? (left-tree t))) #t)
+        (else #f)
+  )
+)
+
+; example
+(define a (make-tree 3 (make-tree 4 empty-tree empty-tree) (make-tree 5 empty-tree empty-tree)))
+(tree? a)
+(tree? '(1 2 3))
+
+(define example (make-tree 1
+                           (make-tree 3 (make-leaf 8) empty-tree)
+                           (make-tree 7 empty-tree (make-tree 9
+                                                              (make-leaf 10)
+                                                              (make-leaf 11)))))
+
+(define example-bst (make-tree 8
+                               (make-tree 3 (make-leaf 1) (make-tree 6 (make-leaf 4) (make-leaf 7)))
+                               (make-tree 10 empty-tree (make-tree 14 (make-leaf 13) empty-tree))))
+
+example
+example-bst
+
+(define empty-tree? null?)
+
+; искаме да проверим дали нещо се среща в дърво
+; във всяка от задачите започваме с проверка за празно дърво
+; елемент се среща в дърво, тогава когато:
+; - или е корена на дървото
+; - или се среща в лявото, или дясното поддърво (среща се = member-tree?)
+(define (member-tree? m t) ; assuming the tree is valid
+  (cond ((empty-tree? t) #f)
+        ((equal? (root-tree t) m) #t)
+         (else
+          (or
+           (member-tree? m (left-tree t))
+           (member-tree? m (right-tree t))))
+  )
+)
+
+(member-tree? 4 example-bst)
