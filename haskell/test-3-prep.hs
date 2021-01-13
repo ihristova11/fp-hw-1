cd1d14f18e15433db2a2947a458a9bd0450342bf
Irina Hristova <hristova119@gmail.com>
2021-01-13 20:31:32 +0200

    Update test-3-prep.hs

--- haskell/test-3-prep.hs
+++ haskell/test-3-prep.hs
@@ -1,4 +1,4 @@
---maximum 
+-- maximum 
 maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "empty list"
 maximum' [x] = x
@@ -12,9 +12,28 @@ maximum'' [] = error "losho"
 maximum'' [x] = x
 maximum'' (x:xs) = max x (maximum' xs)
 
---replicate
+-- replicate
 replicate' :: (Eq a) => Int -> a -> [a]
 replicate' n x 
     | n <= 0 = []
     | otherwise  = x : replicate (n - 1) x
 
+-- take
+take' :: (Num a, Ord a) => a -> [a] -> [a]
+take' 0 xs = []
+take' n (x:xs) = x : take' (n - 1) xs 
+
+--reverse
+reverse' :: (Ord a, Eq a) => [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+-- repeat
+repeat' :: a -> [a]  
+repeat' x = x:repeat' x 
+
+-- zip 
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [x] [y] = [(x,y)]
+zip' (x:xs) (y:ys) = (x,y): zip' xs ys
