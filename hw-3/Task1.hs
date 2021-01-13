e3c18d617607334ea580b6c4dbd16b4ed6234b8f
Irina Hristova <hristova119@gmail.com>
2021-01-14 00:10:22 +0200

    Update Task1.hs

---
+++ hw-3/Task1.hs
@@ -0,0 +1,36 @@
+module Task1 where
+
+data Tree a = EmptyTree | Node {
+                            value :: a,
+                            left  :: Tree a,
+                            right :: Tree a
+                          } deriving (Show,Read)
+
+data Strategy = Inorder | Postorder | Preorder deriving (Show, Read, Eq)
+
+inorder :: Tree a -> [a]
+inorder EmptyTree = []
+inorder (Node val l r) = inorder l ++ [val] ++ inorder r
+
+preorder :: Tree a -> [a]
+preorder EmptyTree = []
+preorder (Node val l r) = [val] ++ preorder l ++ preorder r
+
+postorder :: Tree a -> [a]
+postorder EmptyTree = []
+postorder (Node val l r) = postorder l ++ postorder r ++ [val]
+
+values :: Strategy -> (Tree a) -> [a]
+values s t 
+    | s == Inorder = inorder t
+    | s == Postorder = postorder t
+    | s == Preorder = preorder t
+
+testTree = Node 5
+                (Node 12
+                      EmptyTree
+                      (Node 9 EmptyTree EmptyTree))
+                (Node (-6)
+                      (Node 20 EmptyTree EmptyTree)
+                      (Node 2 EmptyTree EmptyTree))
+
