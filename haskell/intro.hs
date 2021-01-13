0aa105f5fd4bc3cdfb0edad86e1f29f6dfd0edc1
Irina Hristova <hristova119@gmail.com>
2021-01-13 13:12:59 +0200

    Update intro.hs

--- haskell/intro.hs
+++ haskell/intro.hs
@@ -7,13 +7,13 @@ mySum :: Int -> Int -> Int
 mySum a b = a + b
 
 factorial :: Int -> Int 
-factorial n = if n == 0 then 1 else (factorial (n - 1)) * n
+factorial n = if n == 0 then 1 else factorial (n - 1) * n
 
 -- pattern matching (preffered because of readability and etc.)
 fact :: Int -> Int 
 fact 0 = 1
 fact 1 = 1
-fact n = (fact (n - 1)) * n
+fact n = fact (n - 1) * n
 
 --fibonacci
 fib :: Int -> Int 
@@ -22,7 +22,7 @@ fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
 sumElements :: [Int] -> Int 
-sumElements xs = if null xs then 0 else (head xs) + sumElements (tail xs)
+sumElements xs = if null xs then 0 else head xs + sumElements (tail xs)
 
 sumElements' :: [Int] -> Int 
 sumElements' [] = 0
