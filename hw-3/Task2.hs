0db0b6ae23bc4b4570939cd58ea7e61135c9a22b
Irina Hristova <hristova119@gmail.com>
2021-01-14 10:22:58 +0200

    Update Task2.hs

--- hw-3/Task2.hs
+++ hw-3/Task2.hs
@@ -1,7 +1,6 @@
 module Task2 where 
 
 import Data.Word
-
 data Rgb = Rgb { red   :: Word8
                , green :: Word8
                , blue  :: Word8 } deriving (Show,Read)
@@ -10,11 +9,10 @@ data Image = Image { width   :: Int
                    , height  :: Int
                    , content :: [[Rgb]] } deriving (Show,Read)
 
-greyscaleF :: Rgb -> Rgb 
-greyscaleF (Rgb r g b) = Rgb val val val
-    where val = (0.30 * r) + (0.59 * g) + (0.11 * b)
+-- A) grayscale
+grayscaleF :: Rgb -> Rgb 
+grayscaleF (Rgb r g b) = Rgb val val val
+    where val = round ((0.30 * toRational r) + (0.59 * toRational g) + (0.11 * toRational b))
 
 grayscale :: Image -> Image
-grayscale (Image w h c) = Image w h (map (map greyscaleF) c)
-
--- мап на всеки елемент, за всеки елемент пак мап и за всяко от стойностите прилагаме горната формула
+grayscale (Image w h c) = Image w h (map (map grayscaleF) c)
